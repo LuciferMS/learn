89eb1192cf025d558b56747d66b8707350ca54a7
LuciferMS <2039176261@qq.com>
2019-05-30 18:53:18 +0800

    lombok测试

---
+++ src/test/java/lombok/ILombokTest.java
@@ -0,0 +1,5 @@
+package lombok;
+
+public interface ILombokTest {
+    void sayHello();
+}
---
+++ src/test/java/lombok/LombokTester.java
@@ -0,0 +1,11 @@
+package lombok;
+
+import org.junit.Test;
+
+public class LombokTester {
+    @Test
+    public void valTest(){
+        val test = new LombokTestClass();
+        test.subMed();
+    }
+}
