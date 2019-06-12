30cc8e30ace145d0b7647623fffdb72cc078506d
LuciferMS <2039176261@qq.com>
2019-06-12 13:29:32 +0800

    '实现最小栈以及添加包信息'

---
+++ src/main/java/algorithm/leetcode/Solution155.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description: leetcode155 最小栈
+ * @author: Elliot
+ * @create: 2019-06-12 13:07
+ **/
+public class Solution155 {
+}
---
+++ src/main/java/algorithm/leetcode/Solution56.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description: leetcode56
+ * @author: Elliot
+ * @create: 2019-06-12 08:43
+ **/
+public class Solution56 {
+}
