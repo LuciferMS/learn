cb61c416330df0981bce536f8895f7d1ef0ad5d5
LuciferMS <2039176261@qq.com>
2019-06-20 22:12:32 +0800

    '单调栈'

---
+++ src/main/java/algorithm/leetcode/Solution496.java
@@ -0,0 +1,10 @@
+package algorithm.leetcode;
+
+/**
+ * @program: learn
+ * @description: leetcode496
+ * @author: Elliot
+ * @create: 2019-06-19 16:27
+ **/
+public class Solution496 {
+}
---
+++ src/main/java/algorithm/leetcode/Solution503.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description: leetcode503 单调栈
+ * @author: Elliot
+ * @create: 2019-06-20 21:41
+ **/
+public class Solution503 {
+}
