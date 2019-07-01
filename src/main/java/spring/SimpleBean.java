8ff5e1f757baffc1f97b37bce94b7e64377f2a1c
LuciferMS <2039176261@qq.com>
2019-07-01 17:46:06 +0800

    'spring'

---
+++ src/main/java/spring/SimpleBean.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description: Spring测试Bean
+ * @author: Elliot
+ * @create: 2019-06-30 23:04
+ **/
+public class SimpleBean {
+}
---
+++ src/test/java/spring/beans/BeanTest.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description: spring bean测试
+ * @author: Elliot
+ * @create: 2019-06-30 23:06
+ **/
+public class BeanTest {
+}
