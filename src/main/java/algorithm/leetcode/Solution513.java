54f73daee6277972aca419360e60cac10f908eab
LuciferMS <2039176261@qq.com>
2019-07-18 16:25:12 +0800

    'leetcode广度优先遍历'

---
+++ src/main/java/algorithm/leetcode/Solution513.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description: Solution513 广度优先遍历1
+ * @author: Elliot
+ * @create: 2019-07-18 15:04
+ **/
+public class Solution513 {
+}
---
+++ src/main/java/algorithm/leetcode/Solution994.java
@@ -0,0 +1,10 @@
+package algorithm.leetcode;
+
+/**
+ * @program: learn
+ * @description: Solution994 广度优先遍历--腐烂的橘子
+ * @author: Elliot
+ * @create: 2019-07-18 15:31
+ **/
+public class Solution994 {
+}
