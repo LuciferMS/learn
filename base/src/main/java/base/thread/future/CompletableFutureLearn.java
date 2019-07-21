54e214325d61407461957ee57edb257256c8a9d5
LuciferMS <2039176261@qq.com>
2019-07-21 16:53:06 +0800

    'CompletableFutureLearn'

---
+++ base/src/main/java/base/thread/future/CompletableFutureLearn.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description: CompletableFutureLearn 异步任务接口
+ * @author: Elliot
+ * @create: 2019-07-21 16:38
+ **/
+public class CompletableFutureLearn {
+}
---
+++ base/src/main/java/base/thread/future/FutureTask.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description: future
+ * @author: Elliot
+ * @create: 2019-07-21 16:33
+ **/
+public class FutureTask {
+}
