ece92205388056fc0920d3fe5982792dd4999d5a
LuciferMS <2039176261@qq.com>
2019-07-28 11:26:19 +0800

    'readmee.md'

---
+++ base/src/main/java/com/learn/base/java/enmutation/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @program: learn
+ * @description: 枚举
+ * @author: Elliot
+ * @create: 2019-07-28 01:54
+ **/
+package com.learn.base.java.enmutation;
--- base/src/main/java/com/learn/base/java/exception/ExceptionTest.java
+++ base/src/main/java/com/learn/base/java/exception/ExceptionTest.java
@@ -1,4 +1,4 @@
-package base;
+package com.learn.base.exception;
 
 import java.util.List;
 
