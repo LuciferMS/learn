d6787a3febc6b5d2775b4edf3ffcc5e2642cd9d5
LuciferMS <2039176261@qq.com>
2019-08-01 07:30:55 +0800

    '静态类型检查'

---
+++ base/src/main/java/com/learn/base/java/others/IntegerTest.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description: IntegerTest
+ * @author: Elliot
+ * @create: 2019-08-01 07:28
+ **/
+public class IntegerTest {
+}
---
+++ base/src/main/java/com/learn/base/java/others/StaticCheck.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description: 静态类型检查
+ * @author: Elliot
+ * @create: 2019-08-01 06:48
+ **/
+public class StaticCheck {
+}
