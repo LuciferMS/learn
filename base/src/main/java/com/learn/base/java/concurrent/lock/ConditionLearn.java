b3a83d7f76a81e1d4927f8cf3052c810aa5f843c
LuciferMS <2039176261@qq.com>
2019-08-05 21:42:49 +0800

    'condition学习和使用condition交替打印数组'

---
+++ base/src/main/java/com/learn/base/java/concurrent/lock/ConditionLearn.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description: condition使用
+ * @author: Elliot
+ * @create: 2019-08-05 20:30
+ **/
+public class ConditionLearn {
+}
---
+++ base/src/main/java/com/learn/base/java/concurrent/thread/ThreadPrint2.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description: 使用condition交替打印数字
+ * @author: Elliot
+ * @create: 2019-08-05 20:47
+ **/
+public class ThreadPrint2 {
+}
