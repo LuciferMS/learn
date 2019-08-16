ad7e2fc6d4433de294dc1b701ac4eb69b65a9021
LuciferMS <2039176261@qq.com>
2019-08-16 10:53:59 +0800

    'stringbuffer'

---
+++ base/src/main/java/com/learn/base/java/others/MethodTest2.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description:
+ * @author: Elliot
+ * @create: 2019-08-15 23:30
+ **/
+public class MethodTest2 {
+}
---
+++ base/src/main/java/com/learn/base/java/others/StringBufferTest.java
@@ -0,0 +1,8 @@
+/**
+ * @program: learn
+ * @description: StringBufferTest
+ * @author: Elliot
+ * @create: 2019-08-16 10:48
+ **/
+public class StringBufferTest {
+}
