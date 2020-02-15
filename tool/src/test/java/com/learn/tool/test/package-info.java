fa7a0c723d157eb96778da105b94a371de661ea3
LuciferMS <2039176261@qq.com>
2020-02-15 13:38:10 +0800

    '工具类库'

---
+++ tool/src/test/java/com/learn/tool/test/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @program: learn
+ * @description: 测试工具
+ * @author: Elliot
+ * @create: 2020-02-15 01:47
+ **/
+package com.learn.tool.test;
---
+++ tool/src/test/java/com/learn/tool/test/print/ConsoleTester.java
@@ -0,0 +1,22 @@
+package com.learn.tool.test.print;
+
+import com.learn.tool.print.Console;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: learn
+ * @description: 控制台输出测试类
+ * @author: Elliot
+ * @create: 2020-02-15 01:54
+ **/
+public class ConsoleTester {
+
+    @Test
+    public void printArrayTest(){
+
+    }
+
+}
