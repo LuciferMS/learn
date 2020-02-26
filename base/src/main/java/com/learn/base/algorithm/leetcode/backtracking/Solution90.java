1c0b51d28dd2d8b8a19a0d889313e88341c85b30
LuciferMS <2039176261@qq.com>
2020-02-26 18:43:44 +0800

    '复原ip地址和子集'

---
+++ base/src/main/java/com/learn/base/algorithm/leetcode/backtracking/Solution90.java
@@ -0,0 +1,50 @@
+package com.learn.base.algorithm.leetcode.backtracking;
+
+import com.learn.base.java.others.IntegerTest;
+
+import java.util.*;
+
+/**
+ * @program: learn
+ * @description: 子集2
+ * @author: Elliot
+ * @create: 2020-02-26 14:47
+ **/
+public class Solution90 {
+
+    private List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+    private int[] nums;
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        if(nums == null || nums.length == 0) return result;
+        this.nums = nums;
+        Arrays.sort(nums);
+        helper(0, new ArrayDeque<>());
+        return result;
+    }
+
+    private void helper(int index, Deque<Integer> deque){
+        if (index > nums.length){
+            result.add(new ArrayList<>(deque));
+            return ;
+        }
+        result.add(new ArrayList<>(deque));
+        for(int i = index; i < nums.length; i++){
+            deque.add(nums[i]);
+            helper(i + 1 , deque);
+            deque.removeLast();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution90 solution90 = new Solution90();
+        List<List<Integer>> result = solution90.subsetsWithDup(new int[]{1, 2, 2});
+        for(List<Integer> list : result){
+            for(int num : list){
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ base/src/main/java/com/learn/base/algorithm/leetcode/backtracking/Solution93.java
@@ -0,0 +1,20 @@
+package com.learn.base.algorithm.leetcode.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: learn
+ * @description: 复原ip地址
+ * @author: Elliot
+ * @create: 2020-02-26 15:08
+ **/
+public class Solution93 {
+
+    private List<String> result = new ArrayList<>();
+
+    public List<String> restoreIpAddresses(String s) {
+        if(s == null) return result;
+        return result;
+    }
+}
