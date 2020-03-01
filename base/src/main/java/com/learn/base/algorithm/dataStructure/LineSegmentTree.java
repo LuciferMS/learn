17abe98b4949744a855c04fcd2cb261bcd178fcf
LuciferMS <2039176261@qq.com>
2020-03-01 12:03:06 +0800

    '线段树'

---
+++ base/src/main/java/com/learn/base/algorithm/dataStructure/LineSegmentTree.java
@@ -0,0 +1,15 @@
+package com.learn.base.algorithm.dataStructure;
+
+/**
+ * @program: learn
+ * @description: 线段树
+ * @author: Elliot
+ * @create: 2020-03-01 01:22
+ **/
+public class LineSegmentTree {
+
+    class Node{
+
+    }
+
+}
