2b6f7323bbc4823c85350d607359ec7235eee044
LuciferMS <2039176261@qq.com>
2020-03-02 14:10:32 +0800

    'codePointTest'

---
+++ base/src/test/java/base/CharTest.java
@@ -0,0 +1,18 @@
+package base;
+
+import org.junit.Test;
+
+/**
+ * @program: learn
+ * @description: 字符测试类
+ * @author: Elliot
+ * @create: 2020-03-02 14:05
+ **/
+public class CharTest {
+
+    @Test
+    public void toCodePointTest(){
+        System.out.println(Character.toCodePoint('a', 'b'));
+    }
+
+}
