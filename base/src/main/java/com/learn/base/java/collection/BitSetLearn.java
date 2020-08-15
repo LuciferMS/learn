b2fa92b1239318218370ce46f783af0972544e60
Elliot <2039176261@qq.com>
2020-08-15 11:31:07 +0800

    '.'

---
+++ base/src/main/java/com/learn/base/java/collection/BitSetLearn.java
@@ -0,0 +1,10 @@
+package com.learn.base.java.collection;
+
+import java.util.BitSet;
+
+public class BitSetLearn {
+    public static void main(String[] args) {
+        BitSet set = new BitSet(129);
+        set.set(1);
+    }
+}
